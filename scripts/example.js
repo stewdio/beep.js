f3359110e463ac71d8b9f967871830839664cc6e
Stewart <stewart@stewd.io>
2015-03-30 00:24:09 -0400

    Because example.

---
+++ scripts/example.js
@@ -0,0 +1,25 @@
+
+
+
+
+document.addEventListener( 'DOMContentLoaded', function(){
+
+
+	//  This is it -- making an Instrument is this easy!
+	//  That’s because of our “batteries includied” approach.
+
+	window.synth = new BEEP.Instrument( 'synth' )
+
+
+	//  The	trick now is customization.
+	//  Go forth and customize!
+	//  Overwrite the instance methods!
+	//  Overwrite the prototypes!
+})
+
+
+
+
+
+
+
